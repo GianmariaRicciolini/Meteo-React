dc85ae3032917c47bd6b61e93f38f56fb2b3e811
Giovanni Maria Ricciolini <151955659+GianmariaRicciolini@users.noreply.github.com>
2024-03-24 18:28:39 +0100

    carousel layout update

--- src/components/CarouselDays.jsx
+++ src/components/CarouselDays.jsx
@@ -8,12 +8,12 @@ const CarouselDays = ({ weatherData }) => {
   const uniqueDateObjects = uniqueDays.map((day) => new Date(day));
 
   return (
-    <Carousel interval={null}>
+    <Carousel interval={null} prevIcon={null} nextIcon={null} indicators={false}>
       {uniqueDateObjects.map((day, index) => (
         <Carousel.Item key={index}>
           <Row>
             <Col>
-              <h5 className="card-title text-center">{day.toDateString()}</h5>
+              <h5 className="card-title text-center fs-big my-3">{day.toDateString()}</h5>
               <OneDay weatherData={weatherData} day={day} />
             </Col>
           </Row>
--- src/components/DataPerHour.jsx
+++ src/components/DataPerHour.jsx
@@ -1,30 +1,42 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import { ThermometerHalf, Wind, Moisture } from "react-bootstrap-icons";
 
 const DataPerHour = ({ day }) => {
   const kelvinToCelsius = (tempKelvin) => {
     return (tempKelvin - 273.15).toFixed(0);
   };
   return (
-    <Row className="d-flex align-items-baseline">
-      <Col>
+    <Row className="d-flex align-items-center bg-info bg-opacity-10 border border-info-subtle border-start-0 rounded">
+      <Col xs={1} className="text-end">
+        <h6 className="fw-bold pt-2">{day.dt_txt.split(" ")[1].slice(0, -3)}</h6>
+      </Col>
+
+      <Col xs={2} className="text-end">
         <img src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt="Weather Icon" />
       </Col>
-      <Col>{day.dt_txt.split(" ")[1].slice(0, -3)}</Col>
       <Col>
-        <p>{day.weather[0].main}</p>
+        <p className="pt-3 fw-bold">{day.weather[0].main}</p>
       </Col>
-      <Col>
-        <p>Temperature: {kelvinToCelsius(day.main.temp)}°C</p>
+
+      <Col xs={3}>
+        <p className="pt-3">
+          Temp. <ThermometerHalf /> : <span className="fw-bold">{kelvinToCelsius(day.main.temp)}°C</span>
+        </p>
+
+        <p>
+          Perceived <ThermometerHalf /> : <span className="fw-bold">{kelvinToCelsius(day.main.feels_like)}°C</span>
+        </p>
       </Col>
-      <Col>
-        <p>Feels like: {kelvinToCelsius(day.main.feels_like)}°C</p>
-      </Col>
-      <Col>
-        <p>Wind: {day.wind.speed} m/s</p>
-      </Col>
-      <Col>
-        <p>Humidity: {day.main.humidity}%</p>
+
+      <Col xs={3}>
+        <p className="pt-3">
+          Wind <Wind /> : <span className="fw-bold">{day.wind.speed} m/s</span>
+        </p>
+
+        <p>
+          Humidity <Moisture /> : <span className="fw-bold">{day.main.humidity}%</span>
+        </p>
       </Col>
     </Row>
   );
